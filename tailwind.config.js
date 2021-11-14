7c09a0b8caecc97e93a99097ccaff8f05bd35dd0
kabeh2 <mattcnk@hotmail.com>
2021-11-14 18:34:46 -0500

    Tailwind setup.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// https://www.pullrequest.com/blog/how-to-create-a-nextjs-typescript-tailwindcss-project/
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
